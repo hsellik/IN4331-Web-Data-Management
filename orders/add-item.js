d8dd6a016c20c1e0def7e85d85ec485e8d84c755
Martijn de Heus <mjdeheus@hotmail.com>
2019-05-21 10:48:32 +0200

    Add order functions

---
+++ orders/add-item.js
@@ -0,0 +1,56 @@
+// TODO
+
+'use strict';
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: "us-east-1"});
+
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
+  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1"});
+
+  let responseBody = "";
+  let statusCode = 0;
+
+  const { order_id } = event.pathParameters;
+  const { item_id } = event.pathParameters;
+
+  const searchOrderParams = {
+    TableName: "orders",
+    Key: {
+      id: order_id
+    }
+  };
+
+  // TODO: Check if item exists and stock!
+
+  try {
+    const updateParams = {
+      TableName: 'orders',
+      Key: { id: order_id },
+      ReturnValues: 'ALL_NEW',
+      UpdateExpression: 'set #items = list_append(if_not_exists(#items, :empty_list), :item_id)',
+      ExpressionAttributeNames: {
+        '#items': 'items'
+      },
+      ExpressionAttributeValues: {
+        ':item_id': [item_id],
+        ':empty_list': []
+      }
+    }
+    const data = await documentClient.update(updateParams).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 200;
+  } catch (err) {
+    responseBody = "Something went wrong."
+    statusCode = 403;
+  }
+  
+  const response = { 
+    statusCode: statusCode,
+    body: responseBody
+  };
+  
+  return response;
+
+};
---
+++ orders/create-order.js
@@ -0,0 +1,55 @@
+'use strict';
+const AWS = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
+
+AWS.config.update({ region: "us-east-1"});
+
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
+  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1"});
+
+  let responseBody = "";
+  let statusCode = 0;
+
+  const uuid = uuidv4();
+  const { user_id } = event.pathParameters;
+
+  const searchParams = {
+    TableName: "Orders",
+    Key: {
+      id: uuid
+    }
+  };
+
+  try {
+    const data = await documentClient.get(searchParams).promise();
+    if (data.Item) {
+      throw 'Duplicate ID tried to be written.';
+    } else {
+      const putParams = {
+        TableName: "Orders",
+        Item: {
+          id: uuid,
+          user_id: user_id,
+          items: [],
+          total_price: 0,
+        }
+      };
+      const data = await documentClient.put(putParams).promise();
+      responseBody = uuid;
+      statusCode = 200;
+    }  
+  } catch (err) {
+    console.log(err)
+    responseBody = "Something went wrong."
+    statusCode = 403;
+  }
+  
+  const response = { 
+    statusCode: statusCode,
+    body: responseBody
+  };
+  
+  return response;
+
+};
