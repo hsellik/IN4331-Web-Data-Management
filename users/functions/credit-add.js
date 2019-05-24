e8e19cee752be6761f32d9634acd7621d4105068
joy <joy_yin@outlook.com>
2019-05-25 01:25:47 +0200

    restore deleted files

---
+++ users/functions/credit-add.js
@@ -0,0 +1,71 @@
+console.log('Starting credit add function');
+
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient({region: 'PLACEHOLDER_REGION'});
+
+exports.handler = async function(e, ctx) {
+
+    const user_id = ((e.pathParameters || {})['user_id']) || e.user_id;
+    const amountRaw = ((e.pathParameters || {})['amount']) || e.amount;
+
+    const amount = parseInt(amountRaw, 10);
+    if (amount == NaN) { // TODO: This doesn't seem to work, not sure why
+        callback(null, {
+            statusCode: 400,
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ message: `Amount parameter is not a number: \"${amountRaw}\"` }),
+        });
+        return;
+    }
+
+    const getParams = {
+        TableName: 'Users',
+        Key: {
+            User_ID : user_id,
+        }
+    };
+    try {
+        const data = await dynamoDB.get(getParams).promise();
+        if (data.Item == null) {
+            return {
+                statusCode: 404,
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({ Message: "User not found" }),
+            }
+        } else if (data.Item.credit === null) {
+            return {
+                statusCode: 500,
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({ Message: "credit not found in User item.", item: data.Item }),
+            }
+        } else {
+            try {
+                const updateParams = {
+                    TableName: 'Users',
+                    Key: { User_ID: user_id },
+                    UpdateExpression: "SET credit = credit + :amount",
+                    ExpressionAttributeValues: { ":amount": amount },
+                    ReturnValues: "ALL_NEW"
+                }
+                const data = await dynamoDB.update(updateParams).promise();
+                return {
+                    statusCode: 200,
+                    headers: { "Content-type": "application/json" },
+                    body: JSON.stringify({ ...data.Attributes }),
+                };
+            } catch (err) {
+                return {
+                    statusCode: 500,
+                    headers: { "Content-type": "application/json" },
+                    body: JSON.stringify({ Message: err }),
+                }
+            }
+        }
+    } catch (err) {
+        return {
+            statusCode: 500,
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ Message: err }),
+        }
+    }
+};
---
+++ users/functions/remove-user.js
@@ -0,0 +1,45 @@
+console.log('Starting remove user function');
+
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient({region: 'PLACEHOLDER_REGION'});
+
+exports.handler = async function(e, ctx) {
+
+    const user_id = ((e.pathParameters || {})['user_id']) || (e.user_id);
+
+    const params = {
+        TableName: 'Users',
+        Key: {
+            User_ID : user_id,
+        }
+    };
+
+    try {
+        const data = await dynamoDB.get(params).promise();
+        if (data.Item == null) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ Message: "User not found" })
+            }
+        } else {
+            try {
+                const data = await dynamoDB.delete(params).promise();
+                return {
+                    statusCode: 200,
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ Message: `Deleted user ${user_id}` }),
+                }
+            } catch (err) {
+                return {
+                    statusCode: 500,
+                    body: JSON.stringify({ Message: err }),
+                }
+            }   
+        }
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ Message: err }),
+        }
+    }
+};
